ce1ecd46d43978da41fb3be83fc1a86fc26ec8ed
LottaL <lotta.s.laukkanen@gmail.com>
2019-10-04 13:59:37 +0300

    cors attempt 2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*" );
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
