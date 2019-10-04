62d9ae8614251048d51ba65bd4327daf8cd536c5
LottaL <lotta.s.laukkanen@gmail.com>
2019-10-04 13:52:46 +0300

    cors attempt 1

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ const fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get("/", async (req, res) => {
     res.status(200).send("Welcome to PetSpy database API endpoint!")
 });
